f12666e880c203699676cac412e0e9ea76a74b94
zhangsan <zhangsan@qq.com>
2019-03-17 16:18:33 +0800

    终于找到提交图标了！

--- Demo01/src/main/java/com/work/java/JavaDemo01.java
+++ Demo01/src/main/java/com/work/java/JavaDemo01.java
@@ -6,5 +6,12 @@ public class JavaDemo01 {
         System.out.println("222222");
 
         System.out.println("333333");
+
+        System.out.println("终于找到提交图标了！！！");
+        System.out.println("终于找到提交图标了！！！");
+        System.out.println("终于找到提交图标了！！！");
+        System.out.println("终于找到提交图标了！！！");
+
+
     }
 }
